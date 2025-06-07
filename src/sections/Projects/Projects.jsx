3d5b5284b557b14182cc400dd049f4873476f78c
mizunate-tech <140473977+mizunate-tech@users.noreply.github.com>
2025-06-07 23:45:04 +0800

    Project-Section
    Adding Project Section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import styles from '../Projects/ProjectStyles.module.css'
+import viberr from '../../assets/viberr.png'
+import freshBurger from '../../assets/fresh-burger.png'
+import ProjectCard from '../../common/ProjectCard';
 function Projects() {
   return (
     <section id="projects" className={styles.container}> 
     <h1 className="sectionTitle">Projects</h1>
     {/* Projects Section*/}
     <div className={styles.projectsContainer}>
-        <a href="https://github.com/"></a>
+       <ProjectCard 
+       src={viberr} 
+       link="https://github.com"
+       h3="Viberr"
+        p= "Streaming App"
+       />
+        <ProjectCard 
+       src={freshBurger} 
+       link="https://github.com"
+       h3="Fresh Burger"
+        p= "Hamburger Restaurant"
+       />
     </div>
     </section >
   );    
