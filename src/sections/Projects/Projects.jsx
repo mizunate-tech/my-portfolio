a4461d206a6b039dd8dd3d4c2c06e96887f6ecd4
mizunate-tech <140473977+mizunate-tech@users.noreply.github.com>
2025-06-28 16:52:08 +0800

    Sql-EDA-Project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../Projects/ProjectStyles.module.css";
 import datawarehouse from "../../assets/data-warehouse.png";
-import freshBurger from "../../assets/fresh-burger.png";
+import sql_eda from "../../assets/sql-eda.png";
 import ProjectCard from "../../common/ProjectCard";
 function Projects() {
   return (
@@ -13,7 +13,13 @@ function Projects() {
           src={datawarehouse}
           link="https://github.com/mizunate-tech/sql-data-warehouse-project.git"
           h3="Data Warehouse and Analytics"
-          p=" Develop a modern data warehouse using SQL Server to consolidate sales data, enabling analytical reporting and informed decision-making."
+          p="Develop a modern data warehouse using SQL Server to consolidate sales data, enabling analytical reporting and informed decision-making."
+        />
+        <ProjectCard
+          src={sql_eda}
+          link="https://github.com/mizunate-tech/sql-data-warehouse-project.git"
+          h3="SQL-Data-Analytics"
+          p="A comprehensive collection of SQL scripts for data exploration, analytics, and reporting. "
         />
       </div>
     </section>
