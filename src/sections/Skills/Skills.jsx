cee06bea7a402e94d3f1ba4f5405a12f4f408c14
mizunate-tech <140473977+mizunate-tech@users.noreply.github.com>
2025-07-11 11:56:28 +0800

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -12,6 +12,7 @@ function Skills() {
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
+      <h5>Languages</h5>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="SQL" />
         <SkillList src={checkMarkIcon} skill="Excel" />
