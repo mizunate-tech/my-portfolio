d717a3abd6a6ae2df9a4b5c2b677470ed8561093
mizunate-tech <140473977+mizunate-tech@users.noreply.github.com>
2025-07-11 13:27:51 +0800

    Edited Skill and Resume

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -12,11 +12,11 @@ function Skills() {
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
-      <h5>Languages</h5>
+      <h2>Data Analysis</h2>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="SQL" />
         <SkillList src={checkMarkIcon} skill="Excel" />
-        <SkillList src={checkMarkIcon} skill="Tableau" />
+        <SkillList src={checkMarkIcon} skill="PowerBI" />
         <SkillList src={checkMarkIcon} skill="Python" />
       </div>
       <hr />
